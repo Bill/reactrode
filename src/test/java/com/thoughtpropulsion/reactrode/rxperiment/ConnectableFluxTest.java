2232aaf2e7ef8b40d4847519f9fcfa092ce5ab19
Bill Burcham <bburcham@pivotal.io>
2019-07-02 11:07:13 -0700

    ConnectableFluxTest is cleaner now

--- src/test/java/com/thoughtpropulsion/reactrode/rxperiment/ConnectableFluxTest.java
+++ src/test/java/com/thoughtpropulsion/reactrode/rxperiment/ConnectableFluxTest.java
@@ -1,5 +1,6 @@
 package com.thoughtpropulsion.reactrode.rxperiment;
 
+import static com.thoughtpropulsion.reactrode.Functional.returning;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.time.Duration;
@@ -22,52 +23,95 @@ public class ConnectableFluxTest {
 
     final Flux<Integer> head = Flux.just(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
+    /*
+
+     !! LOOKIE HERE !! this is the important thing
+
+     publish(2) creates the ConnectableFlux that will start producing to all subscribers
+     after the second subscription arrives
+
+     */
     final Flux<Integer> topic = head.publish(2).autoConnect(2);
 
-    final AtomicReference<Subscription> s1 = new AtomicReference<>();
-    final AtomicReference<Subscription> s2 = new AtomicReference<>();
+    final AtomicBoolean fastDone = new AtomicBoolean();
+    final AtomicBoolean fastNeedMore = new AtomicBoolean(true);
+    final AtomicReference<Subscription> fastSubscription =
+        subscribe("Fast!", topic, fastDone, fastNeedMore);
 
-    final AtomicBoolean s1done = new AtomicBoolean();
-    final AtomicBoolean s2done = new AtomicBoolean();
-
-    topic.subscribe(
-        item -> { System.out.println("S1 got item: " + item);},
-        error -> { System.out.println("S1 got error: " + error); s1done.set(true);},
-        () -> { System.out.println("S1 complete."); s1done.set(true);},
-        subscription -> { s1.set(subscription);});
-
-    topic.subscribe(
-        item -> { System.out.println("S2 got item: " + item);},
-        error -> { System.out.println("S2 got error: " + error); s2done.set(true);},
-        () -> { System.out.println("S2 complete."); s2done.set(true);},
-        subscription -> { s2.set(subscription);});
+    final AtomicBoolean slowDone = new AtomicBoolean();
+    final AtomicBoolean slowNeedMore = new AtomicBoolean(true);
+    final AtomicReference<Subscription> slowSubscription =
+        subscribe("slow ", topic, slowDone, slowNeedMore);
 
     final Random random = new Random(1);
 
+    generateRequestsUntilComplete("Fast!", fastSubscription,100, random,
+        fastNeedMore, fastDone);
 
-    generateRequestsForever(s1,1000,random);
-    generateRequestsForever(s2,5000,random);
+    generateRequestsUntilComplete("slow ", slowSubscription,500, random,
+        slowNeedMore, slowDone);
 
-    waitUntilAll(s1done,s2done);
+    waitUntilAll(fastDone,slowDone);
 
     assertThat(true).isTrue();
   }
 
-  static void generateRequestsForever(
+  /*
+   Return ref to Subscription to (ConnectableFlux). This method takes a Flux but that Flux
+   was returned from ConnectableFlux.autoConnect()
+   */
+  private AtomicReference<Subscription> subscribe(
+      final String name,
+      final Flux<Integer> topic,
+      final AtomicBoolean done,
+      final AtomicBoolean needMore) {
+
+    final AtomicReference<Subscription> subscriptionRefs = new AtomicReference<>();
+
+    return returning(subscriptionRefs, subscriptionRef ->
+
+        topic.subscribe(
+
+            item -> {
+              System.out.println(name + " got item: " + item);
+              needMore.set(true);},
+
+            error -> {
+              System.out.println(name + " got error: " + error);
+              done.set(true);},
+
+            () -> {
+              System.out.println(name + " complete.");
+              done.set(true);},
+
+            subscription -> {
+              subscriptionRef.set(subscription);}));
+  }
+
+  /*
+   This sets up a task that issues upstream demand as needed, sleeping periodically.
+   */
+  static void generateRequestsUntilComplete(
+      final String name,
       final AtomicReference<Subscription> subscription,
       final long frequency,
-      final Random random) {
+      final Random random,
+      final AtomicBoolean needMore,
+      final AtomicBoolean complete) {
 
-    System.out.println("requesting 1 on " + subscription);
+    if (needMore.get() && !complete.get()) {
+      needMore.set(false);
+      System.out.println(name + " requesting 1");
+      subscription.get().request(1);
+    }
 
-    subscription.get().request(1);
+    final long delay = gaussianLong(frequency, random);
 
-    final long delay = (long) ((random.nextGaussian() * frequency) + frequency);
-
-    System.out.println("delaying " + delay);
+    System.out.println(name + " delaying " + delay);
     Mono
         .delay(Duration.ofMillis(delay))
-        .doOnNext(_actualDuration -> generateRequestsForever(subscription,frequency,random))
+        .doOnNext(_actualDuration -> generateRequestsUntilComplete(
+            name, subscription,frequency,random, needMore, complete))
         .subscribe();
   }
 
@@ -87,6 +131,10 @@ public class ConnectableFluxTest {
         .of(terms)
         .map(AtomicBoolean::get)
         .reduce(true, Boolean::logicalAnd);
+  }
+
+  private static long gaussianLong(final long range, final Random random) {
+    return (long) (random.nextGaussian() * range + range);
   }
 
 }
