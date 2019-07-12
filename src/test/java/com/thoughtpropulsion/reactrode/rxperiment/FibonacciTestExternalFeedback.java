7dd469f3d76817286e4c86985fba61cc55f85e0c
Bill Burcham <bburcham@pivotal.io>
2019-07-12 15:51:57 -0700

    try stopping w/ Disposable

--- src/test/java/com/thoughtpropulsion/reactrode/rxperiment/FibonacciTestExternalFeedback.java
+++ src/test/java/com/thoughtpropulsion/reactrode/rxperiment/FibonacciTestExternalFeedback.java
@@ -1,7 +1,11 @@
 package com.thoughtpropulsion.reactrode.rxperiment;
 
+import java.util.concurrent.atomic.AtomicReference;
+
 import org.junit.jupiter.api.Test;
 import org.reactivestreams.Processor;
+import reactor.core.Disposable;
+import reactor.core.publisher.EmitterProcessor;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.publisher.UnicastProcessor;
@@ -21,15 +25,21 @@ public class FibonacciTestExternalFeedback {
         .flatMap(pair -> Mono.just(pair.get(0) + pair.get(1)))
         .doOnNext(i -> System.out.println("generated: " + i));
 
+    final AtomicReference<Disposable> stop = new AtomicReference<>();
+
     final Flux<Integer> allValues = Flux
         .concat(Flux.just(0, 1), nextFibonacci)
         .publish()
-        .refCount(2);
+
+        /*
+         refCount(2) isn't what we want because it doesn't disconnect when
+         count drops below 2---it disconnects when count drops below zero
+         */
+//        .refCount(2)
 
         /*
          Without a limitRequest() (or maybe take()), nextFibonacci keeps
-         generating values ahead of the StepVerifier's demand, and the
-         StepVerifier starves---I don't think it ever sees any values.
+         generating values after the StepVerifier finishes.
 
          It's puzzling because I thought the ConnectableFlux produced by
          publish() would throttle down the upstream demand to that of its
@@ -38,11 +48,17 @@ public class FibonacciTestExternalFeedback {
 
         //.limitRequest(6);
 
+        // _this_ is what we need---capture the Disposable for later
+        .autoConnect(2,stop::set);
+
     allValues.subscribe(feedback);
 
     StepVerifier.create(allValues.take(6))
-        .expectNext(0,1,1,2,3,5)
+        .expectNext(0,1,1,2,3,6)
         .expectComplete()
         .verify();
+
+    // ...and stop the flow after the test is done!
+    stop.get().dispose();
   }
 }
