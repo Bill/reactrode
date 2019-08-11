a9ba2abcc64cbfa1c22cde2a241bd77564cb4c04
Bill Burcham <bburcham@pivotal.io>
2019-08-11 14:41:24 -0700

    painted one full 400x400 frame from Flux

--- react-app/src/simple-life.js
+++ react-app/src/simple-life.js
@@ -7,43 +7,67 @@ var mirrorGrid = createArray(gridWidth);
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 ctx.fillStyle = "#FF0000";
+const calculateLocally = false;
 
-// fillRandom(); //create the starting state for the grid by filling it with random cells
 
-console.log("HERE!")
-
+if (calculateLocally) {
+    fillRandom(); //create the starting state for the grid by filling it with random cells
+    function tick() { //main loop
+        // console.time("loop");
+        calculateGridLocally();
+        drawGrid();
+        // console.timeEnd("loop");
+        requestAnimationFrame(tick);
+    }
+    tick(); //call main loop
+} else {
 // Open the connection
-rSocketClient.connect().subscribe(
-    {
-        onComplete: socket => {
-            socket.requestStream({
-                                     data: {},
-                                     metadata: 'all-generations'
-                                 })
-                .subscribe(
-                    {
-                        onComplete: () => console.log('all-generations done'),
-                        onError: error => console.error(error),
-                        onNext: value => console.log(value),
-                        // Nothing happens until `request(n)` is called
-                        onSubscribe: sub => sub.request(5),
-                    }
-                )
-        },
-        onError: error => console.error(error),
-        onSubscribe: cancel => {/* call cancel() to abort */
-        }
-    });
-tick(); //call main loop
+    rSocketClient.connect().subscribe(
+        {
+            onComplete: socket => {
+                socket.requestStream({
+                                         data: {},
+                                         metadata: 'all-generations'
+                                     })
+                    .subscribe(
+                        {
+                            onComplete: () => console.log('all-generations done'),
+                            onError: error => console.error(error),
+                            onNext: value => {
+                                let y = value.data.coordinates.y;
+                                let x = value.data.coordinates.x;
+                                if (value.data.isAlive) {
+                                    theGrid[y][x] = 1;
+                                } else {
+                                    theGrid[y][x] = 0;
+                                }
+                            },
+                            // Nothing happens until `request(n)` is called
+                            onSubscribe: sub => sub.request(400*400),
+                        }
+                    )
+            },
+            onError: error => console.error(error),
+            onSubscribe: cancel => {/* call cancel() to abort */
+            }
+        });
 
-//functions
-function tick() { //main loop
-    // console.time("loop");
-    calculateGridRemotely();
-    drawGrid();
-    // console.timeEnd("loop");
-    requestAnimationFrame(tick);
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    async function foo() {
+        console.log("sleeping...")
+        await sleep(4000)
+        console.log("slept!")
+        drawGrid();
+        requestAnimationFrame(foo);
+    }
+
+    foo();
+
 }
+
 
 function createArray(rows) { //creates a 2 dimensional array of required height
     var arr = [];
@@ -54,6 +78,7 @@ function createArray(rows) { //creates a 2 dimensional array of required height
 }
 
 function fillRandom() { //fill the grid randomly
+    // leave a border so we can see gliders
     for (var j = 100; j < gridHeight - 100; j++) { //iterate through rows
         for (var k = 100; k < gridWidth - 100; k++) { //iterate through columns
             theGrid[j][k] = Math.round(Math.random());
@@ -62,57 +87,46 @@ function fillRandom() { //fill the grid randomly
 }
 
 function drawGrid() { //draw the contents of the grid onto a canvas
-    var liveCount = 0;
+    console.log("drawing")
     ctx.clearRect(0, 0, gridHeight, gridWidth); //this should clear the canvas ahead of each redraw
-    for (var j = 1; j < gridHeight; j++) { //iterate through rows
-        for (var k = 1; k < gridWidth; k++) { //iterate through columns
-            if (theGrid[j][k] === 1) {
-                ctx.fillRect(j, k, 1, 1);
-                liveCount++;
-
+    for (var row = 0; row < gridHeight; row++) { //iterate through rows
+        for (var col = 0; col < gridWidth; col++) { //iterate through columns
+            if (theGrid[row][col] === 1) {
+                ctx.fillRect(row, col, 1, 1);
             }
-        }
-    }
-    // console.log(liveCount/100);
-}
-
-function calculateGridRemotely() {
-    for (var col = 0; col < gridWidth; col++) { //iterate through columns
-        for (var row = 0; row < gridHeight; row++) { //iterate through rows
-
         }
     }
 }
 
 function calculateGridLocally() { //perform one iteration of grid update
 
-    for (var j = 1; j < gridHeight - 1; j++) { //iterate through rows
-        for (var k = 1; k < gridWidth - 1; k++) { //iterate through columns
+    for (var row = 1; row < gridHeight - 1; row++) { //iterate through rows
+        for (var col = 1; col < gridWidth - 1; col++) { //iterate through columns
             var totalCells = 0;
             //add up the total values for the surrounding cells
-            totalCells += theGrid[j - 1][k - 1]; //top left
-            totalCells += theGrid[j - 1][k]; //top center
-            totalCells += theGrid[j - 1][k + 1]; //top right
+            totalCells += theGrid[row - 1][col - 1]; //top left
+            totalCells += theGrid[row - 1][col]; //top center
+            totalCells += theGrid[row - 1][col + 1]; //top right
 
-            totalCells += theGrid[j][k - 1]; //middle left
-            totalCells += theGrid[j][k + 1]; //middle right
+            totalCells += theGrid[row][col - 1]; //middle left
+            totalCells += theGrid[row][col + 1]; //middle right
 
-            totalCells += theGrid[j + 1][k - 1]; //bottom left
-            totalCells += theGrid[j + 1][k]; //bottom center
-            totalCells += theGrid[j + 1][k + 1]; //bottom right
+            totalCells += theGrid[row + 1][col - 1]; //bottom left
+            totalCells += theGrid[row + 1][col]; //bottom center
+            totalCells += theGrid[row + 1][col + 1]; //bottom right
 
             //apply the rules to each cell
             switch (totalCells) {
                 case 2:
-                    mirrorGrid[j][k] = theGrid[j][k];
+                    mirrorGrid[row][col] = theGrid[row][col];
 
                     break;
                 case 3:
-                    mirrorGrid[j][k] = 1; //live
+                    mirrorGrid[row][col] = 1; //live
 
                     break;
                 default:
-                    mirrorGrid[j][k] = 0; //
+                    mirrorGrid[row][col] = 0; //
             }
         }
     }
